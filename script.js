7856db9c3f6dc087bfadfa5188679a67931ecf30
rhuanchueda-creator <rhuan.chueda@escola.pr.gov.br>
2026-09-23 08:36:48 -0300

    aa

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const botao = document.querySelector("#botaoMensagem");
-
-const mensagem = document.querySelector("#mensagemInterativa");
+const botoesCurtir = document.querySelectorALL(".curtir")
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListener("click", curtir);
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;
+    }else{
+        contador.textContent--;
+        curtir = false;
+    }
+}};
